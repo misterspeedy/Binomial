55e7ec354e2466ec3e064fb10e95073170ed87c4
Kit Eason <kit.eason@gmail.com>
2013-10-13 15:02:39 +0100

    Initial version

---
+++ Binomial/Binomial.fs
@@ -0,0 +1,43 @@
+﻿module Binomial
+
+// This algorithm is necessary because the standard formula for Cumulative Binomial Distribution
+// involves calculating and dividing some factorials, which very easily leads to overlow.
+//
+// The algorithm below is Microsoft's solution to this problem in Excel (reference
+// http://support.microsoft.com/kb/827459).  Here we do not bother to use Microsoft's approach
+// of using a different algorithm below n=1030 as the algorithm below appears to produce
+// correct values at any reasonable n.
+//
+
+let CumulativeDistribution (x : int) (n : int) (p : float) =
+    let EssentiallyZero = 10E-12
+    let m = (float(n) * p) |> truncate |> int
+
+    let CalcCurrent value k = 
+        if k > m then 
+            value * float(n - k + 1) * p / (float(k) * (1. - p))
+        else
+            value * float(k + 1) * (1. - p) / (float(n - k) * p)
+
+    let Done k = if k > m then k > n else k < 0
+    let NextK k = if k > m then k + 1 else k - 1
+
+    let rec Calculate k totalUnscaledProbability previous unscaled =
+        let current = CalcCurrent previous k
+        let totalUnscaledProbability' = totalUnscaledProbability + current
+        let unscaled' =
+            if (k <= x) then
+                unscaled + current
+            else
+                unscaled
+        if (Done k) && (current <= EssentiallyZero) then
+            unscaled', totalUnscaledProbability
+        else
+            Calculate (NextK k) totalUnscaledProbability' current unscaled'   
+
+    let InitialUnscaled = if (m <= x) then 1. else 0.
+
+    let UnscaledResultAboveM, TotalUnscaledProbabilityAboveM = Calculate (m+1) 1. 1. InitialUnscaled
+    let UnscaledResult, TotalUnscaledProbability = Calculate (m-1) TotalUnscaledProbabilityAboveM 1. UnscaledResultAboveM
+
+    UnscaledResult / TotalUnscaledProbability
---
+++ BinomialTests/Binomial.Tests.fs
@@ -0,0 +1,25 @@
+﻿module BinomialTests
+
+open System
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>] 
+type ``Given the CumulativeDistribution function`` () =
+    let errorMargin = 1e-10
+
+    [<TestCase(1,1,0.1,1.0000000000)>]
+    [<TestCase(1,10,0.1,0.7360989291)>]
+    [<TestCase(1,100,0.1,0.0003216881)>]
+    [<TestCase(1,100,0.01,0.7357619789)>]
+    [<TestCase(12,345,0.012345,0.9995592084)>]
+    [<TestCase(10,1029,0.02,0.0074346572)>]
+    [<TestCase(10,1030,0.02,0.0073522357)>]
+    [<TestCase(10,1031,0.02,0.0072706707)>]
+    [<TestCase(179,12798,0.012013,0.9797611704)>]
+    [<TestCase(12798,12798,0.012013,1.0)>]
+    member t.
+        ``the function produces the correct result`` (x, n, p, expected) =
+            let actual = Binomial.CumulativeDistribution x n p
+            actual |> should (equalWithin errorMargin) expected
+
